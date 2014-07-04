9555e0a59304d3836d18dfd740ae76ef598963bd
Amir Omidi <amir@aaomidi.com>
2014-07-05 04:25:11 +0430

    ASynced Cleverbot

--- src/main/java/com/aaomidi/dev/skypebot/engine/commands/Chat.java
+++ src/main/java/com/aaomidi/dev/skypebot/engine/commands/Chat.java
@@ -11,7 +11,7 @@ import com.skype.SkypeException;
 import java.util.HashMap;
 
 /**
- * Created by Amir on 7/4/2014.
+ * @author aaomidi
  */
 public class Chat extends SkypeCommand {
     private HashMap<String, ChatterBotSession> bots;
@@ -29,38 +29,43 @@ public class Chat extends SkypeCommand {
     }
 
     @Override
-    public void execute(ChatMessage chatMessage, String[] args) throws SkypeException {
-        try {
-            String senderID = chatMessage.getSenderId();
-            if (args.length == 2) {
-                if (args[1].equalsIgnoreCase("reset")) {
-                    if (bots.containsKey(senderID)) {
-                        bots.remove(senderID);
-                        chatMessage.getChat().send("We reset your skype bot.");
-                        return;
+    public void execute(final ChatMessage chatMessage, final String[] args) throws SkypeException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String senderID = chatMessage.getSenderId();
+                    if (args.length == 2) {
+                        if (args[1].equalsIgnoreCase("reset")) {
+                            if (bots.containsKey(senderID)) {
+                                bots.remove(senderID);
+                                chatMessage.getChat().send("We reset your skype bot.");
+                                return;
+                            }
+                        }
                     }
+                    ChatterBotSession session;
+                    if (bots.containsKey(senderID)) {
+                        session = bots.get(senderID);
+                    } else {
+                        session = chatterBot.createSession();
+                        bots.put(senderID, session);
+                    }
+                    boolean isFirst = true;
+                    StringBuilder sb = new StringBuilder();
+                    for (String arg : args) {
+                        if (!isFirst) {
+                            sb.append(arg);
+                            sb.append(" ");
+                        }
+                        isFirst = false;
+                    }
+                    String message = "Cleverbot: " + session.think(sb.toString());
+                    chatMessage.getChat().send(message);
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
                 }
             }
-            ChatterBotSession session;
-            if (bots.containsKey(senderID)) {
-                session = bots.get(senderID);
-            } else {
-                session = chatterBot.createSession();
-                bots.put(senderID, session);
-            }
-            boolean isFirst = true;
-            StringBuilder sb = new StringBuilder();
-            for (String arg : args) {
-                if (!isFirst) {
-                    sb.append(arg);
-                    sb.append(" ");
-                }
-                isFirst = false;
-            }
-            String message = "Cleverbot: " + session.think(sb.toString());
-            chatMessage.getChat().send(message);
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
+        }).start();
     }
 }
